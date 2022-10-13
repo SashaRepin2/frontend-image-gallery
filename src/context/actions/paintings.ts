844a09d7c81374e6fdbc4549d573b25b576dda7b
Sasha Repin <sasharepin2016@gmail.com>
2022-10-13 10:28:28 +0300

    fixes & add painting action

--- src/context/actions/paintings.ts
+++ src/context/actions/paintings.ts
@@ -1,7 +1,10 @@
+import { IPaintingsState } from "@context/reducers/paintings";
+
 import IPainting from "@interfaces/IPainting";
 
 export const PAINTINGS_ADD_PAINTING = "PAINTINGS/ADD_ADD_PAINTING";
 export const PAINTINGS_ADD_PAINTINGS = "PAINTINGS/ADD_ADD_PAINTINGS";
+export const PAINTINGS_UPDATE = "PAINTINGS/UPDATE";
 
 type TAction<TPayload> = { type: string; payload: TPayload };
 
@@ -14,7 +17,9 @@ export function createAction<TPayload>(type: string): (payload: TPayload) => TAc
 
 export const paintingsAddPaintings = createAction<IPainting[]>(PAINTINGS_ADD_PAINTINGS);
 export const paintingsAddPainting = createAction<IPainting>(PAINTINGS_ADD_PAINTING);
+export const paintingsUpdate = createAction<IPaintingsState>(PAINTINGS_UPDATE);
 
 export type PaintingsActions =
     | ReturnType<typeof paintingsAddPaintings>
-    | ReturnType<typeof paintingsAddPainting>;
+    | ReturnType<typeof paintingsAddPainting>
+    | ReturnType<typeof paintingsUpdate>;
